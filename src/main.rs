1177f89c9b79683dc709bcd089e964a7877baffa
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2025-07-10 17:27:30 -0300

    funciona

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,54 @@
-use std::fmt::format;
+use clap::{arg, Parser};
+use glotaran_converter_lib::{run_das6, run_lfp};
 
-use clap::{Parser, arg}; 
-use glotaran_converter_lib::run;
+pub mod das {
+    pub const PS_PER_NS: f32 = 1000f32;
+    pub const DEFAULT_SYNC_DELAY: f32 = 200.0;
+    pub const DEFAULT_NS_PER_CHN: f32 = 0.055;
+    pub const DEFAULT_OUTPUT_FILENAME: &str = "converted_tres_file.ascii";
+}
+pub mod lfp {}
+
 #[derive(Parser)]
 struct Cli {
     /// Name of the file to convert
-    filename : String,
+    filename: String,
     /// Channels before the excitatory laser pulse, if not indicated default is 200 chn.
     #[arg(short, long, value_name = "DELAY")]
-    sync_delay : Option<f32>,
+    sync_delay: Option<f32>,
     /// Conversion from channels to ns, if not indicated default is 0.055 ns/chn.
     #[arg(short, long, value_name = "CONVERSION_FACTOR")]
-    ns_per_chn : Option<f32>,
+    ns_per_chn: Option<f32>,
     #[arg(short, long)]
-    output_filename : Option<String>
-
+    output_filename: Option<String>,
 }
 
-const PS_PER_NS : f32 = 1000f32;
-const DEFAULT_SYNC_DELAY : f32 = 200.0;
-const DEFAULT_NS_PER_CHN : f32 = 0.055;
-const DEFAULT_OUTPUT_FILENAME : &str = "converted_tres_file.ascii";
+struct LFP<'a> {
+    filename: &'a str,
+}
 
 fn main() {
     let args = Cli::parse();
-    let sync_delay = args.sync_delay.unwrap_or(DEFAULT_SYNC_DELAY);
-    let ns_per_chn = args.ns_per_chn.unwrap_or(DEFAULT_NS_PER_CHN);
-    let output_filename = match args.output_filename.unwrap_or(DEFAULT_OUTPUT_FILENAME.into()) {
+    let sync_delay = args.sync_delay.unwrap_or(das::DEFAULT_SYNC_DELAY);
+    let ns_per_chn = args.ns_per_chn.unwrap_or(das::DEFAULT_NS_PER_CHN);
+    let output_filename = match args
+        .output_filename
+        .unwrap_or(das::DEFAULT_OUTPUT_FILENAME.into())
+    {
         x if x.ends_with("ascii") => x,
-        x => format!("{}.ascii", x)
+        x => format!("{}.ascii", x),
     };
     println!("Corriendo conversion de archivo, {}", args.filename);
     println!("Parámetros");
     println!("Sync delay \t\t\t {} chn", sync_delay);
     println!("Equivalencia \t\t\t {} ns/chn", ns_per_chn);
-    match run_das6(&args.filename, sync_delay, ns_per_chn * PS_PER_NS, output_filename) {
+    match run_das6(
+        &args.filename,
+        sync_delay,
+        ns_per_chn * das::PS_PER_NS,
+        output_filename,
+    ) {
         Ok(filename) => println!("Se convirtió exitosamente el archivo {}", filename),
-        Err(e) => println!("No se pudo convertir el archivo exitosamente porque  {}", e)
+        Err(e) => println!("No se pudo convertir el archivo exitosamente porque  {}", e),
     };
 }
