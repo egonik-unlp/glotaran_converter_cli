5207f631fedc4a91acbaeae74de01eed5d9344fb
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2025-07-11 13:59:48 -0300

    funciona con r4 testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use clap::{arg, Parser};
-use glotaran_converter_lib::{run_das6, run_lfp};
+use anyhow::Context;
+use clap::{arg, Parser, Subcommand};
+use glotaran_converter_lib::{run_das6, run_lfp, run_r4};
 
 pub mod das {
     pub const PS_PER_NS: f32 = 1000f32;
@@ -8,47 +9,90 @@ pub mod das {
     pub const DEFAULT_OUTPUT_FILENAME: &str = "converted_tres_file.ascii";
 }
 pub mod lfp {}
-
-#[derive(Parser)]
+#[derive(Debug, Parser)]
+#[command(
+    name = "glotaran converter util",
+    about = "Utility to convert lfp and das6 tr fluorescence data"
+)]
 struct Cli {
-    /// Name of the file to convert
-    filename: String,
-    /// Channels before the excitatory laser pulse, if not indicated default is 200 chn.
-    #[arg(short, long, value_name = "DELAY")]
+    #[command(subcommand)]
+    command: Commands,
+}
+#[derive(Subcommand, Debug)]
+enum Commands {
+    DAS {
+        /// Name of the file to convert
+        filename: String,
+        /// Channels before the excitatory laser pulse, if not indicated default is 200 chn.
+        #[arg(short, long, value_name = "DELAY")]
+        sync_delay: Option<f32>,
+        /// Conversion from channels to ns, if not indicated default is 0.055 ns/chn.
+        #[arg(short, long, value_name = "CONVERSION_FACTOR")]
+        ns_per_chn: Option<f32>,
+        #[arg(short, long)]
+        output_filename: Option<String>,
+    },
+    LFP {
+        #[arg(short, long, value_name = "filename")]
+        filename: String,
+        #[arg(short, long)]
+        output_filename: Option<String>,
+        #[arg(short = 'r', long, default_value_t = false)]
+        r4: bool,
+    },
+}
+
+fn das6flow(
+    filename: &str,
     sync_delay: Option<f32>,
-    /// Conversion from channels to ns, if not indicated default is 0.055 ns/chn.
-    #[arg(short, long, value_name = "CONVERSION_FACTOR")]
     ns_per_chn: Option<f32>,
-    #[arg(short, long)]
     output_filename: Option<String>,
-}
-
-struct LFP<'a> {
-    filename: &'a str,
-}
-
-fn main() {
-    let args = Cli::parse();
-    let sync_delay = args.sync_delay.unwrap_or(das::DEFAULT_SYNC_DELAY);
-    let ns_per_chn = args.ns_per_chn.unwrap_or(das::DEFAULT_NS_PER_CHN);
-    let output_filename = match args
-        .output_filename
-        .unwrap_or(das::DEFAULT_OUTPUT_FILENAME.into())
-    {
+) -> anyhow::Result<()> {
+    let sync_delay = sync_delay.unwrap_or(das::DEFAULT_SYNC_DELAY);
+    let ns_per_chn = ns_per_chn.unwrap_or(das::DEFAULT_NS_PER_CHN);
+    let output_filename = match output_filename.unwrap_or(das::DEFAULT_OUTPUT_FILENAME.into()) {
         x if x.ends_with("ascii") => x,
-        x => format!("{}.ascii", x),
+        x => format!("{x}.ascii"),
     };
-    println!("Corriendo conversion de archivo, {}", args.filename);
+    println!("Corriendo conversion de archivo, {filename}");
     println!("Parámetros");
-    println!("Sync delay \t\t\t {} chn", sync_delay);
-    println!("Equivalencia \t\t\t {} ns/chn", ns_per_chn);
-    match run_das6(
-        &args.filename,
+    println!("Sync delay \t\t\t {sync_delay} chn");
+    println!("Equivalencia \t\t\t {ns_per_chn} ns/chn");
+    run_das6(
+        filename,
         sync_delay,
         ns_per_chn * das::PS_PER_NS,
         output_filename,
-    ) {
-        Ok(filename) => println!("Se convirtió exitosamente el archivo {}", filename),
-        Err(e) => println!("No se pudo convertir el archivo exitosamente porque  {}", e),
-    };
+    )
+    .context("Error convirtiendo archivo de fluorescencia")?;
+
+    return anyhow::Ok(());
+}
+
+fn lfpflow(filename: &str, _output_filename: Option<String>, r4: bool) -> anyhow::Result<()> {
+    if r4 {
+        run_r4(filename.to_owned()).context("Problema con r4 file")?;
+    } else {
+        run_lfp(filename).context("Problem running file")?;
+    }
+    return anyhow::Ok(());
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Cli::parse();
+    match args.command {
+        Commands::LFP {
+            filename,
+            output_filename,
+            r4,
+        } => lfpflow(&filename, output_filename, r4),
+        Commands::DAS {
+            filename,
+            sync_delay,
+            ns_per_chn,
+            output_filename,
+        } => das6flow(&filename, sync_delay, ns_per_chn, output_filename),
+    }
+    .context("Error convirtiendo datos:")?;
+    return anyhow::Ok(());
 }
